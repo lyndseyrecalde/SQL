ec0b826210f88260ca8a63a9f056dda4d64f9140
lyndseyrecalde <lyndseycraw@aol.com>
2024-07-18 12:56:11 -0700

    Update Superstore_Project

--- Superstore_Project.sql
+++ Superstore_Project.sql
@@ -1,1 +1,28 @@
+SELECT item_name, price
+FROM superstore
+ORDER by price;
 
+SELECT SUM(price)
+FROM superstore;
+
+SELECT SUM(price*stock_quantity)
+FROM superstore;
+
+SELECT category, SUM(price*stock_quantity)
+FROM superstore
+WHERE category= 'Kitchen Supplies';
+
+SELECT category, average_rating, item_name
+FROM superstore
+WHERE category= 'Kitchen Supplies'
+ORDER BY average_rating;
+
+SELECT category, SUM(price*stock_quantity) as total_name 
+FROM superstore
+GROUP BY category
+ORDER BY total_name DESC;
+
+SELECT category, item_name, MIN(average_rating)
+FROM superstore
+GROUP BY 1, 2
+ORDER BY category, MIN(average_rating) DESC;
